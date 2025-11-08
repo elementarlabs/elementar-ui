5acb4f03abbd30ae67e5bec2e68ccb3e08477fe1
Pavel Salauyou <pavel.salauyou@gmail.com>
2025-11-08 14:56:24 +0100

    feat: add crop shape selection with rectangle and circle support
    - Introduce a dropdown for selecting crop shapes (rectangle or circle).
    - Bind `cropShape` model to dynamically update the crop component.
    - Update UI with Angular Material components for improved user interaction.

--- src/app/components/crop/overview/overview.ts
+++ src/app/components/crop/overview/overview.ts
@@ -1,23 +1,28 @@
-import { Component } from '@angular/core';
+import { Component, model } from '@angular/core';
 import { PageComponent } from '@meta/page/page.component';
 import { PageContentDirective } from '@meta/page/page-content.directive';
 import { PageTitleDirective } from '@meta/page/page-title.directive';
 import { PlaygroundComponent } from '@meta/playground/playground.component';
 import { BasicImageCropExample } from '../_examples/basic-image-crop-example/basic-image-crop-example';
 import { Crop } from '@elementar-ui/components/crop';
+import { FormsModule } from '@angular/forms';
+import { MatFormField } from '@angular/material/form-field';
+import { MatOption, MatSelect } from '@angular/material/select';
 
 @Component({
   imports: [
     PageComponent,
     PageContentDirective,
     PageTitleDirective,
-    PlaygroundComponent,
-    BasicImageCropExample,
-    Crop
+    Crop,
+    MatFormField,
+    MatSelect,
+    MatOption,
+    FormsModule
   ],
   templateUrl: './overview.html',
   styleUrl: './overview.scss'
 })
 export class Overview {
-
+  cropShape = model<'rectangle' | 'circle'>('rectangle');
 }
